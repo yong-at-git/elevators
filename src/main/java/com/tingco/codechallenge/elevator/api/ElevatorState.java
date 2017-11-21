aa5b91898658feb66906c2cdcfb68c3f1e528c40
Yong Huang <yong.e.huang@gmail.com>
2017-11-21 18:52:50 +0100

    Introduce states for the elevator FCM modelling.

---
+++ src/main/java/com/tingco/codechallenge/elevator/api/ElevatorState.java
@@ -0,0 +1,27 @@
+package com.tingco.codechallenge.elevator.api;
+
+/**
+ * Created by Yong Huang on 2017-11-21.
+ */
+public enum ElevatorState {
+    /**
+     * Not moving, without pending request and ready for service.
+     */
+    IDLE,
+    /**
+     * At upwards moving.
+     */
+    MOVING_UP,
+    /**
+     * At downwards moving.
+     */
+    MOVING_DOWN,
+    /**
+     * Paused at floor for user to get on or off
+     */
+    STOPPED_FOR_GETTING_ON_OR_OFF,
+    /**
+     * Not moving, without pending request and ready for service;
+     */
+    MAINTENANCE
+}
