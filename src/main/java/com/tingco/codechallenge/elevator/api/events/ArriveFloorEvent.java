0ce83762b3032ce706ed738574da9b14ecb53f64
Yong Huang <yong.e.huang@gmail.com>
2017-11-21 22:35:51 +0100

    Add ArriveFloorEvent and a file to note TODO items.

---
+++ src/main/java/com/tingco/codechallenge/elevator/api/events/ArriveFloorEvent.java
@@ -0,0 +1,39 @@
+package com.tingco.codechallenge.elevator.api.events;
+
+/**
+ * An event that represents the elevator has just arrived a floor.
+ * <p>
+ * Created by Yong Huang on 2017-11-21.
+ */
+public class ArriveFloorEvent {
+    int atFloor;
+
+    public ArriveFloorEvent(int atFloor) {
+        this.atFloor = atFloor;
+    }
+
+    public int getAtFloor() {
+        return atFloor;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        ArriveFloorEvent that = (ArriveFloorEvent) o;
+
+        return atFloor == that.atFloor;
+    }
+
+    @Override public int hashCode() {
+        return atFloor;
+    }
+
+    @Override public String toString() {
+        return "ArriveFloorEvent{" +
+            "atFloor=" + atFloor +
+            '}';
+    }
+}
