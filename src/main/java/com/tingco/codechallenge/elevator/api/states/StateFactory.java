cbc9f451b97c541bbec38b574b7d5be48fe6452b
Yong Huang <yong.e.huang@gmail.com>
2017-11-23 21:09:58 +0100

    Create state factory for conveniently creating state instances.

--- src/main/java/com/tingco/codechallenge/elevator/api/states/StateFactory.java
+++ src/main/java/com/tingco/codechallenge/elevator/api/states/StateFactory.java
@@ -1,7 +1,51 @@
 package com.tingco.codechallenge.elevator.api.states;
 
+import com.tingco.codechallenge.elevator.api.states.impl.DoorClosing;
+import com.tingco.codechallenge.elevator.api.states.impl.DoorOpened;
+import com.tingco.codechallenge.elevator.api.states.impl.DoorOpening;
+import com.tingco.codechallenge.elevator.api.states.impl.JustArrived;
+import com.tingco.codechallenge.elevator.api.states.impl.Maintenance;
+import com.tingco.codechallenge.elevator.api.states.impl.MovingDown;
+import com.tingco.codechallenge.elevator.api.states.impl.MovingUp;
+import com.tingco.codechallenge.elevator.api.states.impl.ReadyToMove;
+
 /**
  * Created by Yong Huang on 2017-11-23.
  */
 public class StateFactory {
+    private StateFactory() {
+        // no arg constructor of factory
+    }
+
+    public static DoorClosing createDoorClosing() {
+        return new DoorClosing();
+    }
+
+    public static DoorOpened createDoorOpened() {
+        return new DoorOpened();
+    }
+
+    public static DoorOpening createDoorOpening() {
+        return new DoorOpening();
+    }
+
+    public static JustArrived createJustArrived() {
+        return new JustArrived();
+    }
+
+    public static Maintenance createMaintenance() {
+        return new Maintenance();
+    }
+
+    public static MovingUp createMovingUp() {
+        return new MovingUp();
+    }
+
+    public static MovingDown createMovingDown() {
+        return new MovingDown();
+    }
+
+    public static ReadyToMove createReadyToMove() {
+        return new ReadyToMove();
+    }
 }
